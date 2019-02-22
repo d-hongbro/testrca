ac1eabdc74128847577ef0406ab43f03f44e504d
David <davidhong342@gmail.com>
2019-02-21 17:32:15 -0800

    Changed endpoints for registration

--- hello/api/urls.py
+++ hello/api/urls.py
@@ -5,14 +5,14 @@ from .views import AttendeeRudView, RegistrationRudView, EventRudView, TicketRud
 from .views import AttendeeCreateView, EventCreateView, TicketCreateView, RegistrationCreateView, TransactionCreateView, ReferenceCreateView, LoggingCreateView
 
 urlpatterns = [
-	path('attendee/', AttendeeCreateView.as_view(), name="attendee-create"),
+	path('registration/', AttendeeCreateView.as_view(), name="attendee-create"),
 	path('attendee/<int:pk>/', AttendeeRudView.as_view(), name="attendee-crud"),
 	path('event/', EventCreateView.as_view(), name="event-create"),
 	path('event/<int:pk>/', EventRudView.as_view(), name="event-crud"),
 	path('ticket/', TicketCreateView.as_view(), name="ticket-create"),
 	path('ticket/<int:pk>/', TicketRudView.as_view(), name="ticket-crud"),
-	path('registration/', RegistrationCreateView.as_view(), name="registration-create"),
-	path('registration/<int:pk>/', RegistrationRudView.as_view(), name="registration-crud"),
+	# path('registration/', RegistrationCreateView.as_view(), name="registration-create"),
+	# path('registration/<int:pk>/', RegistrationRudView.as_view(), name="registration-crud"),
 	path('transaction/', TransactionCreateView.as_view(), name="transaction-create"),
 	path('transaction/<int:pk>/', TransactionRudView.as_view(), name="transaction-crud"),
 	path('reference/', ReferenceCreateView.as_view(), name="reference-create"),
